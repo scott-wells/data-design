6fc0ad23f9d9e1d289929d9a94aa53e78d988d2b
Scott Wells <efwells101@gmail.com>
2019-01-16 13:54:48 -0700

    create text content for persona and use case

---
+++ epic/use-case.php
@@ -0,0 +1,72 @@
+/* PERSONA
+Name: Caleb Navarro
+Age: 28
+Gender: Male
+Ethnicity: Mixed Race
+Occupation: Video Producer/Audio Engineer
+Hobbies: Enjoys traveling, visiting unique places, and new restaurants. Visits friends often at the beach or in trendy
+	city hotspots. Goes on road trips. Also enjoys, hanging out at home in his sweatpants and watching Netflix. Occasionally
+	orders from Ubereats or Grubhub.
+Technology: Laptop - MacOSX 10.14, 2018 15in Macbook Pro 2.6GHz, 512GB Storage, 16GB 2400 MHz DD4 RAM, Retina Display;
+	Phone: iOS 12, iPhoneX, 5.8" Retina Display, 64GB Storage, 3D Touch
+Technology Comfort Level: Power user, very comfortable with his devices
+Attitude: Creates personal videos for his Instagram and Youtube channel. Also, freelances and creates videos for small business clients.
+	Active social media user. Power user of the Mac OSX operating system and iOS. Creates his own music, as well as mixes stage sound
+	professionally. Enjoys supporting new music artists, and does not like corporate, cookie cutter songs.
+Technological Needs: Needs a one-stop website to find high-quality, original, copyright-free music tracks for his video projects.
+Needs/Goals/Features:
+	- Wants the ability to find unique, fresh, high-quality, copyright-free songs online at a reasonable price
+	- Doesn't have the time to endlessly search the web for music that fits his criteria
+	- Wants to preview music before purchasing, as well as edit the song in industry-standard software
+	- Wants to edit a song to a video montage he has created for his Youtube and Instagram channels
+	- Needs to quickly find songs by genre, mood, tempo, instrumentation, and song length.
+	- Prefers multiple versions of a song for video intros, bumpers, and longer formats - 15sec, 30sec, full-length, etc
+	- Wants to be able to buy a song using online payment methods
+	- Needs multiple compression options for the song he is downloading - .aiff, .wmv, .mp3, etc
+	- Likes a simple, clean, minimal design aesthetic as opposed to something more colorful and complicated - soundstripe.com, audiojungle.net
+	- Wants to be able to create playlists of favorites for later use
+Frustrations:
+ 	- Frustrated a the low quality and talent of other stock music sites - audiojungle.net, stockmusic.net, audioblocks.com
+ 	- Frustrated that it is difficult to contact, pay, and receive music copyrights from talented artists on other music
+	websites - soundcloud.com
+	- Frustrated that songs don't come in different lengths for easy editing for video - stockmusic.net
+*/
+
+// USER STORY - As a user, Caleb would like to find and pay for copyright free music for his video project.
+
+/* USE CASE
+Title: Purchasing a copyright-free song
+Description: Caleb wants to purchase a high-quality, copyright-free music track from Musicbed.com
+User Name & Their Role: Caleb; video editor who is searching for copyright-free music on the Internet
+Usage Preconditions: Must have a Musicbed account and be logged in
+Usage Postconditions: Song is downloaded as an audio file to Caleb's computer
+Interaction Flow:
+	- Caleb enters musicbed.com into his web browser
+	- Server returns musicbed.com's homepage
+	- Caleb clicks "Browse" on the main page navigation bar
+	- Server returns the "Songs" page
+	- Caleb enters a keyword into the search field at the top of the page
+	- Server saves the keyword as a search parameter at the top of the page and returns a list of songs that meet all the parameter criteria
+	- Caleb add an additional keyword
+	- Server save the keyword as a search parameter at the top of the page and returns a list of songs that meet all the parameter criteria
+	- Caleb browses the "Songs" page by scrolling and sees the list various music tracks, their album art, genre, dynamic range,
+	song length, as well as options to save to a playlist or add to cart
+	- Server highlights songs on hover and presents the option to preview a track via an icon over the album art
+	- Caleb selects a music track that looks interesting to him and clicks the "preview" button
+	- Server pops up a music player at the bottom of the browser with various options and controls - album art, song name,
+	artist name, previous, play, and next track buttons, audio scrub control slider, song timestamp, volume slider, more options menu,
+	download button, add to cart button, and tech support menu.
+	- Caleb uses the controls to preview the track
+	- Caleb likes the song and decides to purchase the track - he clicks the add to cart button
+	- Server pops up a window with the preview options available on the "Songs" page and requires selection from a series of
+	drop down menus in order to add to cart - project type, type, distribution, size
+	- Caleb selects the appropriate types that fit his project from the drop-down menus
+	- Server activates the add to cart button and displays the price of the song
+	- Caleb clicks add to cart
+	- Server displays a window with the same song preview options and info, displays the final price, cart total, a
+	"Continue Browsing" button and a "Checkout" button
+	- Caleb clicks the "Checkout" button
+	- Server displays a window "My Cart" with purchase details
+	- Caleb follows the prompts and completes his purchase
+Frequency of Use: Once every week
+*/
