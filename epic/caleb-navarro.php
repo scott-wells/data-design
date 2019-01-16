e16f8114724059a43a14363fd544beb70a2966d3
Scott Wells <efwells101@gmail.com>
2019-01-16 14:56:39 -0700

    fixed problem with img src reference

--- epic/caleb-navarro.php
+++ epic/caleb-navarro.php
@@ -15,22 +15,22 @@
 		<br/>
 
 		<ul>
-			<li><h3>Name:</h3> Caleb Navarro</li>
-			<li><h3>Age:</h3> 28</li>
-			<li><h3>Gender:</h3> Male</li>
-			<li><h3>Ethnicity:</h3> Mixed Race</li>
-			<li><h3>Occupation:</h3> Video Producer/Audio Engineer</li>
-			<li><h3>Hobbies:</h3> Enjoys traveling, visiting unique places, and new restaurants. Visits friends often at the beach or in trendy
+			<li><strong>Name:</strong> Caleb Navarro</li>
+			<li><strong>Age:</strong> 28</li>
+			<li><strong>Gender:</strong> Male</li>
+			<li><strong>Ethnicity:</strong> Mixed Race</li>
+			<li><strong>Occupation:</strong> Video Producer/Audio Engineer</li>
+			<li><strong>Hobbies:</strong> Enjoys traveling, visiting unique places, and new restaurants. Visits friends often at the beach or in trendy
 				city hotspots. Goes on road trips. Also enjoys, hanging out at home in his sweatpants and watching Netflix. Occasionally
 				orders from Ubereats or Grubhub.</li>
-			<li><h3>Technology:</h3> Laptop - MacOSX 10.14, 2018 15in Macbook Pro 2.6GHz, 512GB Storage, 16GB 2400 MHz DD4 RAM, Retina Display;
+			<li><strong>Technology:</strong> Laptop - MacOSX 10.14, 2018 15in Macbook Pro 2.6GHz, 512GB Storage, 16GB 2400 MHz DD4 RAM, Retina Display;
 				Phone: iOS 12, iPhoneX, 5.8" Retina Display, 64GB Storage, 3D Touch</li>
-			<li><h3>Technology Comfort Level:</h3> Power user, very comfortable with his devices</li>
-			<li><h3>Attitude:</h3> Creates personal videos for his Instagram and Youtube channel. Also, freelances and creates videos for small business clients.
+			<li><strong>Technology Comfort Level:</strong> Power user, very comfortable with his devices</li>
+			<li><strong>Attitude:</strong> Creates personal videos for his Instagram and Youtube channel. Also, freelances and creates videos for small business clients.
 				Active social media user. Power user of the Mac OSX operating system and iOS. Creates his own music, as well as mixes stage sound
 				professionally. Enjoys supporting new music artists, and does not like corporate, cookie cutter songs.</li>
-			<li><h3>Technological Needs:</h3> Needs a one-stop website to find high-quality, original, copyright-free music tracks for his video projects.</li>
-			<li><h3>Needs/Goals/Features:</h3>
+			<li><strong>Technological Needs:</strong> Needs a one-stop website to find high-quality, original, copyright-free music tracks for his video projects.</li>
+			<li><strong>Needs/Goals/Features:</strong>
 				<ul style="list-style-type: circle;">
 					<li>Wants the ability to find unique, fresh, high-quality, copyright-free songs online at a reasonable price</li>
 					<li>Doesn't have the time to endlessly search the web for music that fits his criteria</li>
@@ -42,7 +42,7 @@
 					<li>Likes a simple, clean, minimal design aesthetic as opposed to something more colorful and complicated</li>
 					<li>Wants to be able to create playlists of favorites for later use</li>
 				</ul></li>
-			<li><h3>Frustrations:</h3>
+			<li><strong>Frustrations:</strong>
 			<ul style="list-style-type: circle;">
 				<li>Frustrated a the low quality and talent of other stock music sites - audiojungle.net, stockmusic.net, audioblocks.com</li>
 				<li>Frustrated at wasting time on the Internet endlessly searching for music that fits his criteria</li>
---
+++ epic/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Data Design Project</title>
+	</head>
+
+	<body>
+
+		<h1>Data Design Project</h1>
+
+		<?php include "caleb-navarro.php"; ?>
+
+	</body>
+</html>
