408d87995689a554137a276ffea1e331bcb2577b
Scott Wells <efwells101@gmail.com>
2019-01-16 15:44:45 -0700

    begin creating conceptual map

---
+++ epic/conceptual-model.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Data Design Project</title>
+	</head>
+
+	<body>
+
+		<p><strong>User</strong></p>
+		<ul>
+			<li>userId</li>
+			<li>userEmail</li>
+			<li>userPassword</li>
+		</ul>
+
+		<p><strong>Track</strong></p>
+		<ul>
+			<li>trackId</li>
+			<li>trackGenregenreId</li>
+			<li>trackMoodmoodId</li>
+			<li>trackInstrumentsinstrumentsId</li>
+			<li>trackLengthlengthId</li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+
+		</ul>
+
+		<p><strong>User Favorites</strong></p>
+		<ul>
+			<li>userFavortiesuserId</li>
+			<li>userFavoritessongId</li>
+			<li>userFavoritesalbumId</li>
+		</ul>
+
+		<p><strong>Cart</strong></p>
+		<ul>
+			<li>cartId</li>
+			<li></li>
+			<li>userPassword</li>
+		</ul>
+
+	</body>
+</html>
