e9067d8143da9a3a68c991478d4fd97d21fc2bf8
Scott Wells <efwells101@gmail.com>
2019-01-16 14:11:12 -0700

    create markup for user persona

--- epic/use-case.php
+++ epic/use-case.php
@@ -1,36 +1,4 @@
-/* PERSONA
-Name: Caleb Navarro
-Age: 28
-Gender: Male
-Ethnicity: Mixed Race
-Occupation: Video Producer/Audio Engineer
-Hobbies: Enjoys traveling, visiting unique places, and new restaurants. Visits friends often at the beach or in trendy
-	city hotspots. Goes on road trips. Also enjoys, hanging out at home in his sweatpants and watching Netflix. Occasionally
-	orders from Ubereats or Grubhub.
-Technology: Laptop - MacOSX 10.14, 2018 15in Macbook Pro 2.6GHz, 512GB Storage, 16GB 2400 MHz DD4 RAM, Retina Display;
-	Phone: iOS 12, iPhoneX, 5.8" Retina Display, 64GB Storage, 3D Touch
-Technology Comfort Level: Power user, very comfortable with his devices
-Attitude: Creates personal videos for his Instagram and Youtube channel. Also, freelances and creates videos for small business clients.
-	Active social media user. Power user of the Mac OSX operating system and iOS. Creates his own music, as well as mixes stage sound
-	professionally. Enjoys supporting new music artists, and does not like corporate, cookie cutter songs.
-Technological Needs: Needs a one-stop website to find high-quality, original, copyright-free music tracks for his video projects.
-Needs/Goals/Features:
-	- Wants the ability to find unique, fresh, high-quality, copyright-free songs online at a reasonable price
-	- Doesn't have the time to endlessly search the web for music that fits his criteria
-	- Wants to preview music before purchasing, as well as edit the song in industry-standard software
-	- Wants to edit a song to a video montage he has created for his Youtube and Instagram channels
-	- Needs to quickly find songs by genre, mood, tempo, instrumentation, and song length.
-	- Prefers multiple versions of a song for video intros, bumpers, and longer formats - 15sec, 30sec, full-length, etc
-	- Wants to be able to buy a song using online payment methods
-	- Needs multiple compression options for the song he is downloading - .aiff, .wmv, .mp3, etc
-	- Likes a simple, clean, minimal design aesthetic as opposed to something more colorful and complicated - soundstripe.com, audiojungle.net
-	- Wants to be able to create playlists of favorites for later use
-Frustrations:
- 	- Frustrated a the low quality and talent of other stock music sites - audiojungle.net, stockmusic.net, audioblocks.com
- 	- Frustrated that it is difficult to contact, pay, and receive music copyrights from talented artists on other music
-	websites - soundcloud.com
-	- Frustrated that songs don't come in different lengths for easy editing for video - stockmusic.net
-*/
+
 
 // USER STORY - As a user, Caleb would like to find and pay for copyright free music for his video project.
 
