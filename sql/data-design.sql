d8c2c4833df19f05a1ae7cd6a7dad9e389fc966a
Scott Wells <efwells101@gmail.com>
2019-01-17 15:59:36 -0700

    finished sql ddl script

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,4 +1,4 @@
-ALTER DATABASE data-design.sql CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER DATABASE 'data-design.sql' CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 DROP TABLE IF EXISTS cart;
 DROP TABLE IF EXISTS favorites;
@@ -17,27 +17,10 @@ CREATE TABLE `user` (
 
 CREATE TABLE track (
 	trackId BINARY(16) NOT NULL,
-	trackArtistId BINARY(16) NOT NULL,
-	trackAlbumId BINARY(16) NOT NULL,
-	trackGenreId BINARY(16) NOT NULL,
-	trackMoodId BINARY(16) NOT NULL,
-	trackVocalsId BINARY(16) NOT NULL,
-	trackPriceLicenseId BINARY(16) NOT NULL,
 	trackLength DEC(5, 2) NOT NULL,
 	trackName VARCHAR(32),
+	trackPrice DEC(18, 2) NOT NULL,
 	UNIQUE(trackId),
-	INDEX(trackArtistId),
-	INDEX(trackAlbumId),
-	INDEX(trackGenreId),
-	INDEX(trackMoodId),
-	INDEX(trackVocalsId),
-	INDEX(trackPriceLicenseId),
-	FOREIGN KEY(trackArtistId) REFERENCES artist(artistId),
-	FOREIGN KEY(trackAlbumId) REFERENCES album(albumId),
-	FOREIGN KEY(trackGenreId) REFERENCES genre(genreId),
-	FOREIGN KEY(trackMoodId) REFERENCES mood(moodId),
-	FOREIGN KEY(trackVocalsId) REFERENCES vocals(vocalsId),
-	FOREIGN KEY(trackPriceLicenseId) REFERENCES license(licenseId),
 	PRIMARY KEY(trackId)
 );
 
@@ -52,18 +35,12 @@ CREATE TABLE favorites (
 );
 
 CREATE TABLE cart (
-	cartLicenseId BINARY(16) NOT NULL,
 	cartTrackId BINARY(16) NOT NULL,
 	cartUserId BINARY(16) NOT NULL,
-	cartVocalsId BINARY(16) NOT NULL,
 	cartTotal DEC(18, 2) NOT NULL,
-	INDEX(cartLicenseId),
 	INDEX(cartTrackId),
 	INDEX(cartUserId),
-	INDEX(cartVocalsId),
-	FOREIGN KEY(cartLicenseId) REFERENCES license(licenseId),
 	FOREIGN KEY(cartTrackId) REFERENCES track(trackId),
 	FOREIGN KEY(cartUserId) REFERENCES `user`(userId),
-	FOREIGN KEY(cartVocalsId) REFERENCES vocals(vocalsId),
 	PRIMARY KEY(cartTrackId, cartUserId)
 );
