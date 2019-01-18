9b9a5d41c0c55a7b04d6f32315750ad31daf6bb9
Scott Wells <efwells101@gmail.com>
2019-01-18 11:28:07 -0700

    add update statement oml file

--- mysql/oml.sql
+++ mysql/oml.sql
@@ -1,1 +1,7 @@
-INSERT INTO `user` (userId, userActivationToken, userEmail, userHash, userName, userPassword) VALUES (UNHEX(d82d712d2a2e448ca59c8f9a82b90a86), "nananananananananananananananana", email@email.com, "nanananananananananananananananananananananananananananananananananananananananananananananananaa", Joe Smith, );
+INSERT INTO `user` (userId, userActivationToken, userEmail, userHash, userName) VALUES (UNHEX("d82d712d2a2e448ca59c8f9a82b90a86"), "nananananananananananananananana", "email@email.com", "nanananananananananananananananananananananananananananananananananananananananananananananananaa", "Joe Smith");
+
+INSERT INTO `user` (userId, userActivationToken, userEmail, userHash, userName) VALUES (UNHEX("8e8c326f3248414d9d6769f13aff94c4"), "nananananananananananananananana", "email01@email.com", "nanananananananananananananananananananananananananananananananananananananananananananananananaa", "Jane Smith");
+
+INSERT INTO `user` (userId, userActivationToken, userEmail, userHash, userName) VALUES (UNHEX("b7d85b6659fd4176a127030b1aa825be"), "nananananananananananananananana", "email02@email.com", "nanananananananananananananananananananananananananananananananananananananananananananananananaa", "Jody Smith");
+
+UPDATE `user` SET userName = "Marvin Gaye" WHERE userId = UNHEX("b7d85b6659fd4176a127030b1aa825be");
