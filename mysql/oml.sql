d6c332865514b0a8c0640483891a0186eedd0f6b
Scott Wells <efwells101@gmail.com>
2019-01-18 19:55:00 -0700

    insert new record in track table insert foreign key into favorites

--- mysql/oml.sql
+++ mysql/oml.sql
@@ -7,3 +7,8 @@ INSERT INTO `user` (userId, userActivationToken, userEmail, userHash, userName) 
 UPDATE `user` SET userName = 'Marvin Gaye' WHERE userId = UNHEX('8e8c326f3248414d9d6769f13aff94c4');
 
 DELETE FROM `user` WHERE userId = UNHEX('d82d712d2a2e448ca59c8f9a82b90a86');
+
+INSERT INTO track (trackId, trackLength, trackName, trackPrice) VALUES (UNHEX('8f5eebf4f57e46bc9d4d4e873d0b95c8'), 04.17, 'There Will Be Rock', 49.99);
+
+INSERT INTO favorites (favoritesTrackId, favoritesUserId) VALUES (UNHEX('8f5eebf4f57e46bc9d4d4e873d0b95c8'), UNHEX('8e8c326f3248414d9d6769f13aff94c4'));
+
